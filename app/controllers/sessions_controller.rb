e591491ddc646c3f8c8d9930dc168cbeedd5fc17
Ryan Kim <nesteriajik@gmail.com>
2012-07-31 15:58:11 +0900

    Revert "chap 8 exercises"
    This reverts commit 62580e76c1dcf7f870f8fb043a9697359312f11e.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,8 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-      # the session symbol used with form_for 
-      # is no longer being used, so replace params[:session][:email] 
-      # and params[:session][:password] 
-      # with params[:email] params[:password].
-
-      # user = User.find_by_email(params[:session][:email])
-   		 user = User.find_by_email(params[:email])
-      # if user && user.authenticate(params[:session][:password])
-   	 	if user && user.authenticate(params[:password])
+   		 user = User.find_by_email(params[:session][:email])
+   	 	if user && user.authenticate(params[:session][:password])
      		 sign_in user
      		 redirect_to user
    	 	else
